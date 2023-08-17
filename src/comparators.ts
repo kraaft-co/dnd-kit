589d077fb21fa54e1f20c4869c08f175b78b856a
Clauderic Demers <clauderic.d@gmail.com>
2023-08-16 23:03:57 -0400

    Sync latest changes

---
+++ src/comparators.ts
@@ -0,0 +1,23 @@
+export function deepEqual<T>(a: T, b: T) {
+  if (a === b) {
+    return true;
+  }
+
+  if (Array.isArray(a)) {
+    if (!Array.isArray(b)) {
+      return false;
+    }
+
+    if (a.length !== b.length) {
+      return false;
+    }
+
+    const hasDifferentValues = a.some(
+      (value, index) => !deepEqual(value, b[index])
+    );
+
+    return !hasDifferentValues;
+  }
+
+  return JSON.stringify(a) === JSON.stringify(b);
+}
